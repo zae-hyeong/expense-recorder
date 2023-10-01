ae7804ad90fcf8ea6595ff873ae44fcda9199025
zae-hyeong <p0138@naver.com>
2023-10-01 22:53:12 +0900

    [test] add dummy expense data

--- src/App.js
+++ src/App.js
@@ -5,25 +5,13 @@ import NewExpenseForm from './component/NewExpense/NewExpenseForm';
 import Wrapper from './component/UI/Wrapper';
 
 const dummyExpenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
+  { id: 'e1', title: 'Toilet Paper', amount: 5000, date: new Date(2020, 7, 14), },
+  { id: 'e2', title: 'New TV', amount: 890000, date: new Date(2021, 2, 12) },
+  { id: 'e3', title: 'Car Insurance', amount: 1500000, date: new Date(2021, 2, 28), },
+  { id: 'e4', title: 'New Desk (Wooden)', amount: 45000, date: new Date(2021, 5, 12), },
+  { id: 'e5', title: 'Keybord', amount: 120000, date: new Date(2022, 6, 19), },
+  { id: 'e6', title: 'Present', amount: 100000, date: new Date(2023, 12, 31), },
+  { id: 'e7', title: 'Snack', amount: 8000, date: new Date(2023, 11, 12), },
 ];
 
 function App() {
